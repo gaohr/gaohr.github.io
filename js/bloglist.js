63b131a62441bd05d87ceb40b744fc9829524eb9
GaoHR <gaohrgao@163.com>
2016-04-16 22:27:06 +0800

    update

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,5 +1,5 @@
-var blogs = [
-{"title":"使用Github和Disqus建立简单的个人博客", "content":"Github+Disqus建立简单的个人博客", "img":"img/images/ArcGIS.jpg", "date":"2016-04-16","href":"#","tags":["Github","博客","Disqus"]},
-{"title":"ArcGIS创建明确边界矩形矢量面", "content":"xx", "img":"img/images/ArcGIS.jpg", "date":"2016-04-16","href":"#","tags":["Github","博客","Disqus"]},
+var bloglist = [
+{"title":"使用Github和Disqus建立简单的个人博客", "content":"Github+Disqus建立简单的个人博客", "img":"img/images/Github.jpg", "date":"2016-04-15", "href":"#", "comments":"0", "tags":["Github", "博客", "Disqus"]},
+{"title":"ArcGIS创建明确边界矩形矢量面", "content":"xx", "img":"img/images/ArcGIS.jpg", "date":"2016-04-16", "href":"site/blogs/2016-04-16-arcgis-clip-bound.html", "comments":"0", "tags":["ArcGIS", "矢量", "裁剪"]}
 
 ];
--- js/blogsload.js
+++ js/blogsload.js
@@ -1,32 +1,56 @@
+/*
+*Load recent blogs
+*GaoHR
+*2016-04-16
+*/
 $(document).ready(function() {
-		var blogs = eval('blogs');
-		alert("OK");
+		var blogs = eval('bloglist');
 		for(i = 0; i < blogs.length; i++) {
-			alert(blogs.length);
-				for(i = 0; i < data.length; i++) {
-						$("#blog").append(
-							"<div class='blog-page' id='blog'>" +
+				for(i = 0; i < blogs.length; i++) {
+					var tags = readtags(i);
+					$("#blog").append(
+							"<div class='row-fluid'>" +
 							"<div class='span4 blog-tag-data'>" +
-							"<img src='img/images/ArcGIS.jpg' class='blog-img'>" +
+							"<img src='" + blogs[i].img + "' class='blog-img'>" +
 							"<ul class='unstyled inline blog-tags'>" +
 							"<li type='circle'>" +
-							"<i class='icon-tags'></i> " +
-							"<a href='#'>" + data[i].tags[0] + "</a> " +
-							"<a href='#'>矢量</a>" +
-							"<a href='#'>裁剪</a>" +
+							"<i class='icon-tags'></i> " + tags +
 							"</li>" +
 							"</ul>" +
 							"<ul class='unstyled inline'>" +
-							"<li type='circle' class='pull-left'><i class='icon-calendar'></i> <a href='#'>2016-04-15</a></li>" +
-							"<li type='circle' class='pull-right'><i class='icon-comments'></i> <a href='#'>0 Comments</a></li>" +
+							"<li type='circle' class='pull-left'><i class='icon-calendar'></i> <a href='#'>" + blogs[i].date + "</a></li>" +
+							"<li type='circle'><i class='icon-comments'></i> <a href='#'>" + blogs[i].comments + " Comments</a></li>" +
 							"</ul>" +
 							"</div>" +
 							"<div class='span8 blog-article'>" +
-							"<a href='#' class='blog-title'>" + data[i].title + "</a>" +
-							"<p>" + data[i].content + "</p>" +
-							"<a class='btn-blue' href='###'>Read more <i class='icon-circle-arrow-right'></i></a>" +
+							"<a href='" + blogs[i].href + "' class='blog-title'>" + blogs[i].title + "</a>" +
+							"<p>" + blogs[i].content + "</p>" +
+							"<a class='btn-blue' href='" + blogs[i].href + "'>Read more <i class='icon-circle-arrow-right'></i></a>" +
 							"</div>" +
-							"</div>"
+							"</div><hr>"
 						);
 					}
+		}
+		
+		function readtags(n) {
+			tagslist = "";
+			for(t = 0; t < blogs[n].tags.length; t++) {
+				tagslist += "<a href='#'>" + blogs[n].tags[t] + "</a>";
+			}
+			return tagslist;
+		}
 	})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
