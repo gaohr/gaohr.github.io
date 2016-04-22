27b84889d54799fac69806dfe0577897be151664
gaohr <gaohrgao@163.com>
2016-04-22 10:42:24 +0800

    update

--- js/common.js
+++ js/common.js
@@ -9,7 +9,7 @@ $("#relatedlinks").append("<ul class=\"blog-images\">" +
 							"<li><a href=\"###\" target=\"\" title=\"我的简历\"><img alt=\"\" src=\"img/images/resume.png\"></a></li>" +
 							"<li><a href=\"site/zhihu/index.html\" target=\"_blank\" title=\"知乎热门\"><img alt=\"\" src=\"img/images/zhihu.png\"></a></li>" +
 							"<li><a href=\"site/music/index.html\" target=\"_blank\" title=\"小花音乐盒\"><img alt=\"\" src=\"img/images/music.png\"></a></li>" +
-							"<li><a href=\"site/Skills/index.html\" target=\"_blank\" title=\"技能图谱\"><img alt=\"\" src=\"img/images/skills.png\"></a></li>" +
+							"<li><a href=\"site/skills/index.html\" target=\"_blank\" title=\"技能图谱\"><img alt=\"\" src=\"img/images/skills.png\"></a></li>" +
 							"</ul><ul style=\"margin-top:5px;color:#555\">" +
 							"<li type=\"circle\">简历:在线简历</li>" +
 							"<li type=\"circle\">知乎热门：挑选了知乎热门话题，每天早晨6点更新</li>" +
