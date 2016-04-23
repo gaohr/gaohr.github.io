b429ca94f5a8565b5063d2041274965f526ef687
gaohr <gaohrgao@163.com>
2016-04-23 21:44:56 +0800

    update

--- js/common.js
+++ js/common.js
@@ -10,11 +10,12 @@ $("#relatedlinks").append("<ul class=\"blog-images\">" +
 							"<li><a href=\"site/zhihu/index.html\" target=\"_blank\" title=\"知乎热门\"><img alt=\"\" src=\"img/images/zhihu.png\"></a></li>" +
 							"<li><a href=\"site/music/index.html\" target=\"_blank\" title=\"小花音乐盒\"><img alt=\"\" src=\"img/images/music.png\"></a></li>" +
 							"<li><a href=\"site/skills/index.html\" target=\"_blank\" title=\"技能图谱\"><img alt=\"\" src=\"img/images/skills.png\"></a></li>" +
-							"</ul><ul style=\"margin-top:5px;color:#555\">" +
-							"<li type=\"circle\">简历:在线简历</li>" +
-							"<li type=\"circle\">知乎热门：挑选了知乎热门话题，每天早晨6点更新</li>" +
-							"<li type=\"circle\">音乐：休闲时光</li>" +
-							"<li type=\"circle\">技能图谱：将技能串联起来看看吧</li>" +
+							"</ul>" +
+							"<ul style=\"margin-top:5px;color:#555\">" +
+							"<li type=\"circle\">Resume online</li>" +
+							"<li type=\"circle\">Hot topic in Zhihu</li>" +
+							"<li type=\"circle\">Music：Free time</li>" +
+							"<li type=\"circle\">Skills graph</li>" +
 							"</ul><hr>");
 	
 	
