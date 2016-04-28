ed105319aadf5f43a33febc8be99e7da47e2f235
gaohr <gaohrgao@163.com>
2016-04-28 20:15:24 +0800

    update

--- js/common.js
+++ js/common.js
@@ -10,12 +10,14 @@ $("#relatedlinks").append("<ul class=\"blog-images\">" +
 							"<li><a href=\"site/zhihu/index.html\" target=\"_blank\" title=\"知乎热门\"><img alt=\"\" src=\"img/images/zhihu.png\"></a></li>" +
 							"<li><a href=\"site/music/index.html\" target=\"_blank\" title=\"小花音乐盒\"><img alt=\"\" src=\"img/images/music.png\"></a></li>" +
 							"<li><a href=\"site/skills/index.html\" target=\"_blank\" title=\"技能图谱\"><img alt=\"\" src=\"img/images/skills.png\"></a></li>" +
+							"<li><a href=\"site/turing/index.html\" target=\"_blank\" title=\"图灵机器人\"><img alt=\"\" src=\"img/images/turing.png\"></a></li>" +
 							"</ul>" +
 							"<ul style=\"margin-top:5px;color:#555\">" +
 							"<li type=\"circle\">Resume online</li>" +
 							"<li type=\"circle\">Hot topic in Zhihu</li>" +
 							"<li type=\"circle\">Music：Free time</li>" +
 							"<li type=\"circle\">Skills graph</li>" +
+							"<li type=\"circle\">Turing robot</li>" +
 							"</ul><hr>");
 	
 	
