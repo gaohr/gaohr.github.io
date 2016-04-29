dd01b2a218d0d730a20e5bb5c42b23caf2d6fff5
gaohr <gaohrgao@163.com>
2016-04-29 15:11:02 +0800

    update

--- js/common.js
+++ js/common.js
@@ -11,6 +11,7 @@ $("#relatedlinks").append("<ul class=\"blog-images\">" +
 							"<li><a href=\"site/music/index.html\" target=\"_blank\" title=\"小花音乐盒\"><img alt=\"\" src=\"img/images/music.png\"></a></li>" +
 							"<li><a href=\"site/skills/index.html\" target=\"_blank\" title=\"技能图谱\"><img alt=\"\" src=\"img/images/skills.png\"></a></li>" +
 							"<li><a href=\"site/turing/index.html\" target=\"_blank\" title=\"图灵机器人\"><img alt=\"\" src=\"img/images/turing.png\"></a></li>" +
+							"<li><a href=\"site/food/index.html\" target=\"_blank\" title=\"菜谱查询\"><img alt=\"\" src=\"img/images/food.png\"></a></li>" +
 							"</ul>" +
 							"<ul style=\"margin-top:5px;color:#555\">" +
 							"<li type=\"circle\">Resume online</li>" +
@@ -18,6 +19,7 @@ $("#relatedlinks").append("<ul class=\"blog-images\">" +
 							"<li type=\"circle\">Music：Free time</li>" +
 							"<li type=\"circle\">Skills graph</li>" +
 							"<li type=\"circle\">Turing robot</li>" +
+							"<li type=\"circle\">Food</li>" +
 							"</ul><hr>");
 	
 	
