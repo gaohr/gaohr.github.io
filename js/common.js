c49e6436fe7edc5e7ca2b1933b6b6d8ae3283e61
gaohr <gaohrgao@163.com>
2016-05-04 20:40:20 +0800

    update

--- js/common.js
+++ js/common.js
@@ -13,6 +13,7 @@ $("#relatedlinks").append("<ul class=\"blog-images\">" +
 							"<li><a href=\"site/turing/index.html\" target=\"_blank\" title=\"图灵机器人\"><img alt=\"\" src=\"img/images/turing.png\"></a></li>" +
 							"<li><a href=\"site/food/index.html\" target=\"_blank\" title=\"菜谱查询\"><img alt=\"\" src=\"img/images/food.png\"></a></li>" +
 							"<li><a href=\"site/weixin/index.html\" target=\"_blank\" title=\"微信精选\"><img alt=\"\" src=\"img/images/Weixinhot.png\"></a></li>" +
+							"<li><a href=\"site/natgeo/index.html\" target=\"_blank\" title=\"国家地理百年典藏\"><img alt=\"\" src=\"img/images/NationalGeo.png\"></a></li>" +
 							"</ul>" +
 							"<ul style=\"margin-top:5px;color:#555\">" +
 							"<li type=\"circle\">Resume online</li>" +
@@ -22,6 +23,7 @@ $("#relatedlinks").append("<ul class=\"blog-images\">" +
 							"<li type=\"circle\">Turing robot</li>" +
 							"<li type=\"circle\">Food</li>" +
 							"<li type=\"circle\">Hot topic in weixin</li>" +
+							"<li type=\"circle\">National Geographics</li>" +
 							"</ul><hr>");
 	
 	
