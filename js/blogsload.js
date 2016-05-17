b179017e84f611721cd841158d2a5d1c456830b2
gaohr <gaohrgao@163.com>
2016-05-17 13:30:32 +0800

    update

--- js/blogsload.js
+++ js/blogsload.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 			"<a href='" + blogs[i].href + "' class='blog-title'><i class=' icon-bookmark' style='color:#6a6'> </i>" + blogs[i].title + "</a>" +
 			"<a href='Blogs.html#" + blogtype(blogs[i].type) + "' class='blog-type'>[ " + blogs[i].type + " ]</a>" +
 			"<div class='row-fluid'>" +
-			"<div class='span4 blog-tag-data'>" +
+			"<div class='span3 blog-tag-data'>" +
 			"<img src='" + blogs[i].img + "' class='blog-img' style='width:80%;margin-left:10%'>" +
 			"<ul class='unstyled inline'>" +
 			"<li type='circle' class='pull-left'><i class='icon-calendar'></i> <a href='#'>" + blogs[i].date + "</a></li>" +
@@ -26,7 +26,7 @@ $(document).ready(function() {
 			"</small></li>" +
 			"</ul>" +
 			"</div>" +
-			"<div class='span8 blog-article'>" +
+			"<div class='span9 blog-article'>" +
 			"<p>" + blogs[i].content + "</p>" +
 			"<a class='btn-blue' href='" + blogs[i].href + "'>Read more <i class='icon-circle-arrow-right'></i></a>" +
 			"</div>" +
