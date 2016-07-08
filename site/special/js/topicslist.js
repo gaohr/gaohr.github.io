81ce1bfc01203a81c774f35cad95636b09cc183f
gaohr <gaohrgao@163.com>
2016-07-08 12:15:55 +0800

    update

--- site/special/js/topicslist.js
+++ site/special/js/topicslist.js
@@ -2,6 +2,8 @@ var topicslist = [
 
 {"topicid":"2016050801", "title":"母亲节：动物妈妈的母爱", "type":"节日", "content":"真的希望，不再是从电话里说一句：妈妈我爱你，母亲节快乐……", "img":"2016/images/Mothersday.jpg", "date":"2016-05-08", "href":"2016/2016-05-08-happy-mother-s-day.html", "comments":"0"},
 
-{"topicid":"2016051101", "title":"冰与火之歌：世界地图", "type":"节日", "content":"A Song of Ice and Fire", "img":"2016/images/Stark.jpg", "date":"2016-05-11", "href":"2016/2016-05-11-agot-map.html", "comments":"0"},
+{"topicid":"2016051101", "title":"冰与火之歌：世界地图", "type":"兴趣", "content":"A Song of Ice and Fire", "img":"2016/images/Stark.jpg", "date":"2016-05-11", "href":"2016/2016-05-11-agot-map.html", "comments":"17"},
+
+{"topicid":"2016070801", "title":"视频收藏：经典", "type":"收藏", "content":"My favorite videos", "img":"2016/images/Videos.jpg", "date":"2016-07-08", "href":"2016/2016-07-08-videos.html", "comments":"0"},
 
 ];
