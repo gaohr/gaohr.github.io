707398a049677ce2f39e42961f402b95f64c6a69
gaohr <gaohrgao@163.com>
2018-07-05 14:37:14 +0800

    update

--- js/common.js
+++ js/common.js
@@ -6,33 +6,19 @@
 
 //related links
 $("#relatedlinks").html("<ul class=\"blog-images\">" +
-							"<li><a href=\"site/special/index.html\" target=\"\" title=\"专题栏目\"><img alt=\"\" src=\"img/images/SpecialTopics.png\"></a></li>" +
-							"<li><a href=\"###\" target=\"\" title=\"我的简历\"><img alt=\"\" src=\"img/images/resume.png\"></a></li>" +
-							"<li><a href=\"site/zhihu/index.html\" target=\"_blank\" title=\"知乎热门\"><img alt=\"\" src=\"img/images/zhihu.png\"></a></li>" +
-							"<li><a href=\"site/music/index.html\" target=\"_blank\" title=\"小花音乐盒\"><img alt=\"\" src=\"img/images/music.png\"></a></li>" +
-							"<li><a href=\"site/skills/index.html\" target=\"_blank\" title=\"技能图谱\"><img alt=\"\" src=\"img/images/skills.png\"></a></li>" +
-							"<li><a href=\"site/turing/index.html\" target=\"_blank\" title=\"图灵机器人\"><img alt=\"\" src=\"img/images/turing.png\"></a></li>" +
-							"<li><a href=\"site/food/index.html\" target=\"_blank\" title=\"菜谱查询\"><img alt=\"\" src=\"img/images/food.png\"></a></li>" +
-							"<li><a href=\"site/weixin/index.html\" target=\"_blank\" title=\"微信精选\"><img alt=\"\" src=\"img/images/Weixinhot.png\"></a></li>" +
-							"<li><a href=\"site/natgeo/index.html\" target=\"_blank\" title=\"国家地理百年典藏\"><img alt=\"\" src=\"img/images/NationalGeo.png\"></a></li>" +
-							"<li><a href=\"site/war2/index.html\" target=\"_blank\" title=\"二战全史\"><img alt=\"\" src=\"img/images/BBCWar2.png\"></a></li>" +
-							"<li><a href=\"site/graph/index.html\" target=\"_blank\" title=\"函数绘图\"><img alt=\"\" src=\"img/images/Graph.png\"></a></li>" +
-							"<li><a href=\"site/rainbowposter/index.html\" target=\"_blank\" title=\"制作彩虹特效图片\"><img alt=\"\" src=\"img/images/RainbowPoster.png\"></a></li>" +
-							"</ul>" +
-							"<ul style=\"margin-top:5px;color:#555\">" +
-							"<li type=\"circle\">Special Topics</li>" +
-							"<li type=\"circle\">Resume online</li>" +
-							"<li type=\"circle\">Hot topic in Zhihu</li>" +
-							"<li type=\"circle\">Music：Free time</li>" +
-							"<li type=\"circle\">Skills graph</li>" +
-							"<li type=\"circle\">Turing robot</li>" +
-							"<li type=\"circle\">Food</li>" +
-							"<li type=\"circle\">Hot topic in weixin</li>" +
-							"<li type=\"circle\">National Geographics</li>" +
-							"<li type=\"circle\">The World at War</li>" +
-							"<li type=\"circle\">Echarts graph</li>" +
-							"<li type=\"circle\">Rainbow poster special effects</li>" +
-							"</ul><hr>");
+							"<li><a href=\"site/special/index.html\" target=\"\" title=\"专题栏目\"><img alt=\"\" src=\"img/images/SpecialTopics.png\">专题栏目</a></li><br>" +
+							"<li><a href=\"site/zhihu/index.html\" target=\"_blank\" title=\"知乎热门\"><img alt=\"\" src=\"img/images/zhihu.png\">知乎热门</a></li><br>" +
+							"<li><a href=\"site/music/index.html\" target=\"_blank\" title=\"音乐盒\"><img alt=\"\" src=\"img/images/music.png\">音乐盒:休闲时光</a></li><br>" +
+							"<li><a href=\"site/skills/index.html\" target=\"_blank\" title=\"技能图谱\"><img alt=\"\" src=\"img/images/skills.png\">技能图谱</a></li><br>" +
+							"<li><a href=\"site/turing/index.html\" target=\"_blank\" title=\"图灵机器人\"><img alt=\"\" src=\"img/images/turing.png\">图灵-对话机器人</a></li><br>" +
+							"<li><a href=\"site/food/index.html\" target=\"_blank\" title=\"菜谱查询\"><img alt=\"\" src=\"img/images/food.png\">菜谱查询</a></li><br>" +
+							"<li><a href=\"site/weixin/index.html\" target=\"_blank\" title=\"微信精选\"><img alt=\"\" src=\"img/images/Weixinhot.png\">微信精选</a></li><br>" +
+							"<li><a href=\"site/natgeo/index.html\" target=\"_blank\" title=\"国家地理百年典藏\"><img alt=\"\" src=\"img/images/NationalGeo.png\">国家地理百年典藏</a></li><br>" +
+							"<li><a href=\"site/war2/index.html\" target=\"_blank\" title=\"二战全史\"><img alt=\"\" src=\"img/images/BBCWar2.png\">二战全史</a></li><br>" +
+							"<li><a href=\"site/graph/index.html\" target=\"_blank\" title=\"函数绘图\"><img alt=\"\" src=\"img/images/Graph.png\">函数绘图</a></li><br>" +
+							"<li><a href=\"site/rainbowposter/index.html\" target=\"_blank\" title=\"制作彩虹特效图片\"><img alt=\"\" src=\"img/images/RainbowPoster.png\">彩虹特效图片</a></li><br>" +
+							"<li><a href=\"http://remotesensing.top/\" target=\"_blank\" title=\"Remote Sensing\"><img alt=\"\" src=\"img/images/RS_Yi_logo.png\">RemoteSensing.TOP</a></li><br>" +
+							"</ul>");
 	
 // ClustrMaps
 $("#cltmap").html("<script type=\"text/javascript\" id=\"clustrmaps\" src=\"//cdn.clustrmaps.com/map_v2.js?cl=ffffff&w=a&t=tt&d=2MAtWut9LH7lh-B1sQLqIbylbTqh4tSwQyno2E1RAEQ&cmo=cf2500&cmn=00bf00&co=00365c\"></script>");
