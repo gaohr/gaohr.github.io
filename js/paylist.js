f941a8ef4cad5cbe48505eaed5bae123cace242e
gaohr <gaohrgao@163.com>
2018-07-16 14:08:59 +0800

    update

--- js/paylist.js
+++ js/paylist.js
@@ -1,6 +1,7 @@
 var paylist = [
 
-{"name":"*erE", "amount":"2.50", "date":"2018-07-08", "msg":"高哥加油"},
-{"name":"*erE", "amount":"2.33", "date":"2018-07-05", "msg":"赞一个"},
+{"name":"*soud", "amount":"1.00", "date":"2018-07-16", "msg":"Good!"},
+{"name":"*lerE", "amount":"2.50", "date":"2018-07-08", "msg":"高哥加油"},
+{"name":"*lerE", "amount":"2.33", "date":"2018-07-05", "msg":"赞一个"},
 {"name":"*宗昂", "amount":"1.00", "date":"2018-07-05", "msg":"鼓励一下！"}
 ]
