4f85c666a7c292fef8fb5b846b602ad80055228b
gaohr <gaohrgao@163.com>
2018-07-22 02:23:29 +0800

    update

--- site/special/2018/2018-07-21-short-notes/js/notesload.js
+++ site/special/2018/2018-07-21-short-notes/js/notesload.js
@@ -5,12 +5,13 @@ colors = ["#fdd", "#fdf", "#ffd", "#dff", "#ddf", "#dfd"]
 var notes = eval('noteslist');
 	for(i = 0; i < notes.length; i++) {
 		n = parseInt(Math.random()*(colors.length), 10);
+		m = parseInt(Math.random()*(60), 10);
 		$("#zturn").append(
 			"<li class=\"poster-item  zturn-item\" style=\"position:absolute;left:50%;margin-left:-190px;z-index:11;opacity:1;transform:scale(1);background:" + colors[n] + "\">" +
 			"	<p class=\"title\">" + notes[i].title + "</p>" +
 			"	<p class=\"label\">" + notes[i].type + "</p>" +
 				"<div class=\"info\"><span class=\"info-author\"><i class=\"icon-user\"></i>&nbsp;" + notes[i].author + "</span><small>于</small><span class=\"info-loc\">" + notes[i].loc + "</span><span class=\"info-date\">[" + notes[i].date + "]</span></div>" +
-				"<img src=\"./2018-07-21-short-notes/img/img01.jpg\">" +
+				"<img src=\"./2018-07-21-short-notes/img/img" + m.toString() + ".jpg\">" +
 				"<p class=\"content\" style=\"box-shadow:1px 1px 1px " + colors[n] + " inset\">" + notes[i].content + "</p>" +
 				"<div class=\"comment\">" + notes[i].comment + "</div>" +
 			"</li>");
