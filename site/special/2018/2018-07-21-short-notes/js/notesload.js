611f291f9bdaaefb5cdf975781afcabfd50693eb
gaohr <gaohrgao@163.com>
2018-07-22 13:35:56 +0800

    update

--- site/special/2018/2018-07-21-short-notes/js/notesload.js
+++ site/special/2018/2018-07-21-short-notes/js/notesload.js
@@ -10,7 +10,7 @@ for(i = 0; i < noteslist.length; i++) {
 		"	<p class=\"title\">" + noteslist[i].title + "</p>" +
 		"	<p class=\"label\">" + noteslist[i].type + "</p>" +
 			"<div class=\"info\"><span class=\"info-author\"><i class=\"icon-user\"></i>&nbsp;" + noteslist[i].author + "</span><small>于</small><span class=\"info-loc\">" + noteslist[i].loc + "</span><span class=\"info-date\">[" + noteslist[i].date + "]</span></div>" +
-			"<img src=\"./2018-07-21-short-noteslist/img/img" + m + ".jpg\">" +
+			"<img src=\"./2018-07-21-short-notes/img/img" + m + ".jpg\">" +
 			"<p class=\"content\" style=\"box-shadow:1px 1px 1px " + colors[n] + " inset\">" + noteslist[i].content + "</p>" +
 			"<div class=\"comment\">" + noteslist[i].comment + "</div>" +
 		"</li>");
