b085963886cf8ea9000395b2870e117beb24f272
gaohr <gaohrgao@163.com>
2018-08-22 21:48:55 +0800

    update

--- js/paylist.js
+++ js/paylist.js
@@ -1,5 +1,6 @@
 var paylist = [
 
+{"name":"*心安", "amount":"1.00", "date":"2018-08-22", "msg":"厉害了"},
 {"name":"*hong", "amount":"1.00", "date":"2018-07-24", "msg":"么么哒"},
 {"name":"*soud", "amount":"1.00", "date":"2018-07-16", "msg":"Good!"},
 {"name":"*lerE", "amount":"2.50", "date":"2018-07-08", "msg":"高哥加油"},
