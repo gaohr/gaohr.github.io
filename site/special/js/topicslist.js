a0430a2de6d6c8cbf8608a63922ded62d9181a25
gaohr <gaohrgao@163.com>
2018-08-25 14:57:54 +0800

    update

--- site/special/js/topicslist.js
+++ site/special/js/topicslist.js
@@ -6,7 +6,7 @@ var topicslist = [
 
 {"topicid":"2018072101", "title":"雾海拾珠", "type":"情怀", "content":"云雾人生，犹如沧海，听潮起潮落，见人往人来，用诗词绘出触动心灵的每一刻！", "img":"2018/images/ShortNotes.jpg", "date":"2018-07-21", "href":"2018/2018-07-21-short-notes.html", "comments":"0"},
 
-{"topicid":"2016050801", "title":"母亲节：动物妈妈的母爱", "type":"节日", "content":"真的希望，不再是从电话里说一句：妈妈我爱你，母亲节快乐……", "img":"2016/images/Mothersday.jpg", "date":"2016-05-08", "href":"2016/2016-05-08-happy-mother-s-day.html", "comments":"0"},
+{"topicid":"2016050801", "title":"母亲节", "type":"节日", "content":"真的希望，不再是从电话里说一句：妈妈我爱你，母亲节快乐……", "img":"2016/images/Mothersday.jpg", "date":"2016-05-08", "href":"2016/2016-05-08-happy-mother-s-day.html", "comments":"0"},
 
 {"topicid":"2016051101", "title":"冰与火之歌：世界地图", "type":"兴趣", "content":"A Song of Ice and Fire", "img":"2016/images/GOTMap.jpg", "date":"2016-05-11", "href":"2016/2016-05-11-agot-map.html", "comments":"17"},
 
