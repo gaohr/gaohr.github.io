f2bae1c443afcc8fdc39d6268e9f44abf6adee31
gaohr <gaohrgao@163.com>
2018-12-26 16:34:11 +0800

    update

--- site/special/2018/2018-07-21-short-notes/js/notesload.js
+++ site/special/2018/2018-07-21-short-notes/js/notesload.js
@@ -9,7 +9,7 @@ for(i = 0; i < noteslist.length; i++) {
 		"<li class=\"poster-item  zturn-item\" style=\"position:absolute;left:50%;margin-left:-190px;z-index:11;opacity:1;transform:scale(1);background:" + colors[n] + "\">" +
 		"	<p class=\"title\">" + noteslist[i].title + "</p>" +
 		"	<p class=\"label\">" + noteslist[i].type + "</p>" +
-			"<div class=\"info\"><span class=\"info-author\"><i class=\"icon-user\"></i>&nbsp;" + noteslist[i].author + "</span><small>于</small><span class=\"info-loc\">" + noteslist[i].loc + "</span><span class=\"info-date\">[" + noteslist[i].date + "]</span></div>" +
+			"<div class=\"info\"><span class=\"info-author\"><i class=\"icon-user\"></i>" + noteslist[i].author + "</span>于<span class=\"info-loc\">" + noteslist[i].loc + "</span><span class=\"info-date\">[" + noteslist[i].date + "]</span></div>" +
 			"<img src=\"./2018-07-21-short-notes/img/img" + m + ".jpg\">" +
 			"<p class=\"content\" style=\"box-shadow:1px 1px 1px " + colors[n] + " inset\">" + noteslist[i].content + "</p>" +
 			"<div class=\"comment\">" + noteslist[i].comment + "</div>" +
