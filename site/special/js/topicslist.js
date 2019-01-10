38abe094d80f4af6b2918c9595adb6a41b7cf6b6
gaohr <gaohrgao@163.com>
2019-01-10 19:02:22 +0800

    update

--- site/special/js/topicslist.js
+++ site/special/js/topicslist.js
@@ -4,8 +4,6 @@ var topicslist = [
 
 {"topicid":"2016051101", "title":"冰与火之歌：世界地图", "type":"兴趣", "content":"A Song of Ice and Fire", "img":"2016/images/GOTMap.jpg", "date":"2016-05-11", "href":"2016/2016-05-11-agot-map.html", "comments":"17"},
 
-{"topicid":"2018072101", "title":"雾海拾珠", "type":"情怀", "content":"云雾人生，犹如沧海，听潮起潮落，见人往人来，用诗词绘出触动心灵的每一刻！", "img":"2018/images/ShortNotes.jpg", "date":"2018-07-21", "href":"2018/2018-07-21-short-notes.html", "comments":"0"},
-
 {"topicid":"2017041701", "title":"冰与火之歌：九大家族", "type":"兴趣", "content":"A Song of Ice and Fire", "img":"2017/images/GameofThrones.jpg", "date":"2017-04-17", "href":"2017/2017-04-17-nine-families.html", "comments":"0"},
 
 {"topicid":"2016070801", "title":"视频收藏", "type":"收藏", "content":"一些小视频", "img":"2016/images/Videos.jpg", "date":"2016-07-08", "href":"2016/2016-07-08-videos.html", "comments":"0"},
@@ -17,3 +15,5 @@ var topicslist = [
 // {"topicid":"2018072102", "title":"第三场浮生", "type":"情怀", "content":"第一场是白天，第二场是黑夜，第三场属于他……！", "img":"2018/images/TheThirdLife.jpg", "date":"2018-07-21", "href":"2018/2018-07-21-the-third-life.html", "comments":"0"},
 
 // {"topicid":"2018010801", "title":"兔毛价格走势图", "type":"其他", "content":"兔毛价格走势一览", "img":"2018/images/Rabbit.jpg", "date":"2018-01-08", "href":"2018/2018-01-04-price-of-rabbit.html", "comments":"0"},
+
+// {"topicid":"2018072101", "title":"雾海拾珠", "type":"情怀", "content":"云雾人生，犹如沧海，听潮起潮落，见人往人来，用诗词绘出触动心灵的每一刻！", "img":"2018/images/ShortNotes.jpg", "date":"2018-07-21", "href":"2018/2018-07-21-short-notes.html", "comments":"0"},
