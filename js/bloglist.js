1602bef750aefaccd4c08f8067fdfe9d48ad04f1
gaohr <gaohrgao@163.com>
2019-01-10 12:11:11 +0800

    update

--- js/bloglist.js
+++ js/bloglist.js
@@ -24,10 +24,11 @@ var bloglist = [
 
 {"blogid":"2016042001", "title":"VAssistX的安装及在VS 2010中的使用", "type":"专业技术", "author":"小浪", "content":"有时候，我们要编码实现某个功能，或许如行云流水一气呵成，但就是懒得给这代码添注释。等需要改动的时候，那种费力耗时的再去重新理解的过程就不用描述了吧。所以说，规范的代码注释对于开发者来说意义重大，这时，我们的主角+好助手——VAssistX出现了", "img":"site/blogs/2016/images/VAssistX.jpg", "date":"2016-04-20", "href":"http://gaohr.win/site/blogs/2016/2016-04-20-VAssistX.html", "tags":["VAssistX", "VS", "编程"]},
 
-{"blogid":"2016041901", "title":"老树--百年枣树", "type":"随笔小记", "author":"小浪", "content":"这棵枣树，一百三十多岁了，已伴随了六代人的成长……在我家的院子里，有一棵枣树，枝桠茂盛，罩住了半个院子。树干有一抱粗，而且有些倾斜，在其一侧，有一条很深的沟痕，不知是何时何因留下的。树皮龟裂如龙鳞一般，好像在告诉我们它经历了悠久的岁月。……", "img":"site/blogs/2016/images/oldtree.jpg", "date":"2016-04-19", "href":"http://gaohr.win/site/blogs/2016/2016-04-19-the-old-tree.html", "tags":["老树", "感怀", "心情"]},
-
 {"blogid":"2016041601", "title":"ArcGIS创建明确边界矩形矢量面", "type":"专业技术", "author":"小浪", "content":"现有研究区矢量边界图层，要求将该区域遥感影像裁剪，裁剪边界范围距离研究区上下左右范围十公里（10,000m）", "img":"site/blogs/2016/images/ArcGIS.jpg", "date":"2016-04-16", "href":"http://gaohr.win/site/blogs/2016/2016-04-16-arcgis-clip-bound.html", "tags":["ArcGIS", "矢量", "裁剪"]},
 
 {"blogid":"2016041501", "title":"使用Github和Disqus建立简单的个人博客", "type":"专业技术", "author":"小浪", "content":"Github+Disqus建立个人博客,简单实用", "img":"site/blogs/2016/images/Github.jpg", "date":"2016-04-15", "href":"#", "tags":["Github", "博客", "Disqus"]}
 
 ];
+
+
+// {"blogid":"2016041901", "title":"老树--百年枣树", "type":"随笔小记", "author":"小浪", "content":"这棵枣树，一百三十多岁了，已伴随了六代人的成长……在我家的院子里，有一棵枣树，枝桠茂盛，罩住了半个院子。树干有一抱粗，而且有些倾斜，在其一侧，有一条很深的沟痕，不知是何时何因留下的。树皮龟裂如龙鳞一般，好像在告诉我们它经历了悠久的岁月。……", "img":"site/blogs/2016/images/oldtree.jpg", "date":"2016-04-19", "href":"http://gaohr.win/site/blogs/2016/2016-04-19-the-old-tree.html", "tags":["老树", "感怀", "心情"]},
--- site/special/js/topicslist.js
+++ site/special/js/topicslist.js
@@ -2,18 +2,18 @@ var topicslist = [
 
 {"topicid":"2018082501", "title":"走遍中国", "type":"收藏", "content":"五岳奇景，桂林山水，传世建筑……组成了神州大地美不胜收的地理画卷！", "img":"2018/images/GeoChina.jpg", "date":"2018-08-25", "href":"2018/2018-08-25-geo-china.html", "comments":"0"},
 
-{"topicid":"2018072102", "title":"第三场浮生", "type":"情怀", "content":"第一场是白天，第二场是黑夜，第三场属于他……！", "img":"2018/images/TheThirdLife.jpg", "date":"2018-07-21", "href":"2018/2018-07-21-the-third-life.html", "comments":"0"},
+{"topicid":"2016051101", "title":"冰与火之歌：世界地图", "type":"兴趣", "content":"A Song of Ice and Fire", "img":"2016/images/GOTMap.jpg", "date":"2016-05-11", "href":"2016/2016-05-11-agot-map.html", "comments":"17"},
 
 {"topicid":"2018072101", "title":"雾海拾珠", "type":"情怀", "content":"云雾人生，犹如沧海，听潮起潮落，见人往人来，用诗词绘出触动心灵的每一刻！", "img":"2018/images/ShortNotes.jpg", "date":"2018-07-21", "href":"2018/2018-07-21-short-notes.html", "comments":"0"},
 
-{"topicid":"2016050801", "title":"母亲节", "type":"节日", "content":"真的希望，不再是从电话里说一句：妈妈我爱你，母亲节快乐……", "img":"2016/images/Mothersday.jpg", "date":"2016-05-08", "href":"2016/2016-05-08-happy-mother-s-day.html", "comments":"0"},
-
-{"topicid":"2016051101", "title":"冰与火之歌：世界地图", "type":"兴趣", "content":"A Song of Ice and Fire", "img":"2016/images/GOTMap.jpg", "date":"2016-05-11", "href":"2016/2016-05-11-agot-map.html", "comments":"17"},
+{"topicid":"2017041701", "title":"冰与火之歌：九大家族", "type":"兴趣", "content":"A Song of Ice and Fire", "img":"2017/images/GameofThrones.jpg", "date":"2017-04-17", "href":"2017/2017-04-17-nine-families.html", "comments":"0"},
 
 {"topicid":"2016070801", "title":"视频收藏", "type":"收藏", "content":"一些小视频", "img":"2016/images/Videos.jpg", "date":"2016-07-08", "href":"2016/2016-07-08-videos.html", "comments":"0"},
 
-{"topicid":"2017041701", "title":"冰与火之歌：九大家族", "type":"兴趣", "content":"A Song of Ice and Fire", "img":"2017/images/GameofThrones.jpg", "date":"2017-04-17", "href":"2017/2017-04-17-nine-families.html", "comments":"0"},
-
-{"topicid":"2018010801", "title":"兔毛价格走势图", "type":"其他", "content":"兔毛价格走势一览", "img":"2018/images/Rabbit.jpg", "date":"2018-01-08", "href":"2018/2018-01-04-price-of-rabbit.html", "comments":"0"},
+{"topicid":"2016050801", "title":"母亲节", "type":"节日", "content":"真的希望，不再是从电话里说一句：妈妈我爱你，母亲节快乐……", "img":"2016/images/Mothersday.jpg", "date":"2016-05-08", "href":"2016/2016-05-08-happy-mother-s-day.html", "comments":"0"},
 
 ];
+
+// {"topicid":"2018072102", "title":"第三场浮生", "type":"情怀", "content":"第一场是白天，第二场是黑夜，第三场属于他……！", "img":"2018/images/TheThirdLife.jpg", "date":"2018-07-21", "href":"2018/2018-07-21-the-third-life.html", "comments":"0"},
+
+// {"topicid":"2018010801", "title":"兔毛价格走势图", "type":"其他", "content":"兔毛价格走势一览", "img":"2018/images/Rabbit.jpg", "date":"2018-01-08", "href":"2018/2018-01-04-price-of-rabbit.html", "comments":"0"},
