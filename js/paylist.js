829112a2d1a160874a0350cdcbd446b75109d91f
gaohr <gaohrgao@163.com>
2019-02-16 23:11:23 +0800

    update

--- js/paylist.js
+++ js/paylist.js
@@ -1,5 +1,6 @@
 var paylist = [
 
+{"name":"v***o", "amount":"1.00", "date":"2019-02-16", "msg":"Good!"},
 {"name":"***蓝", "amount":"0.10", "date":"2018-12-05", "msg":"Good!"},
 {"name":"*宗昂", "amount":"5.00", "date":"2018-10-09", "msg":"文笔不错"},
 {"name":"*心安", "amount":"1.00", "date":"2018-08-22", "msg":"厉害了"},
