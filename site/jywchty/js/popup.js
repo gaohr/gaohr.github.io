7cf93c21415280d11539743fe0fb4bb029dddd71
gaohr <gaohrgao@163.com>
2019-09-05 12:45:26 +0800

    update

---
+++ site/jywchty/js/popup.js
@@ -0,0 +1,125 @@
+!(function ($, window, document, undefined) {
+
+  var Plugin = function (elem, options) {
+    this.$elem = elem;
+    this.$btn = $('.btn_pop');
+    this.$cont = $('.cont');
+    this.$oMask = $('#mask_shadow');
+    // this.$oTitle = this.$elem.find('.title');
+    // this.$title_text = this.$oTitle.find('p');
+    // this.$close = this.$oTitle.find('span');
+
+    this.b_stop = true; // 防止重复点击
+    this.page_w = $(window).width();
+    this.page_h = $(window).height();
+
+    this.opts = $.extend({}, options);
+  };
+
+  Plugin.prototype = {
+    inital: function () { // 初始化
+      var self = this;
+
+      // this.$title_text.text(this.$title_text.attr('data-title'));
+      this.$elem.css({left: (this.page_w - this.$elem.width()) / 2});
+      this.$elem.css({height: $(window).height() / 1.5});
+      this.$cont.css({height: $(window).height() / 1.75});
+
+      this.$elem.on('click', function () {
+        return false;
+      });
+
+      this.$btn.on('click', function () {
+        self.popbox();
+
+        self.b_stop = false;
+
+        return false;
+      });
+
+      $(document.body).on('click', function () {
+        self.closePopbox();
+      });
+
+    },
+
+    popbox: function () { // 显示弹窗
+      var self = this;
+      
+	  // 于屏幕居中
+      body_width = parseInt($(window).width());
+      body_height = parseInt($(window).height());
+      block_width = parseInt(self.$elem.width());
+      block_height = parseInt(self.$elem.height());
+
+      left_position = parseInt((body_width / 2) - (block_width / 2) + $(window).scrollLeft());
+      if (body_width < block_width) {
+            left_position = 0 + $(window).scrollLeft();
+      };
+
+      top_position = parseInt((body_height / 2) - (block_height / 2) + $(window).scrollTop());
+      if (body_height < block_height) {
+            top_position = 0 + $(window).scrollTop();
+      };
+
+      this.$oMask.show().animate({opacity: 1});;
+      this.$elem.show().animate({opacity: 1, top: top_position}, function () {
+        self.b_stop = true;
+      });
+    },
+
+    closePopbox: function () { // 关闭弹窗
+      var self = this;
+
+      if (this.b_stop) {
+        this.$oMask.animate({opacity: 0}, function () {
+          $(this).hide();
+        });;
+        this.$elem.animate({opacity: 0, top: 150}, function () {
+          $(this).hide();
+        });
+      }
+    },
+
+    constructor: Plugin
+  };
+
+  $.fn.popup = function (options) {
+    var plugin = new Plugin(this, options);
+
+    return plugin.inital();
+  };
+  
+
+})(window.jQuery, window, document);
+
+
+function getCont(id) {
+	item_cont_div = $('#' + id);
+	item_cont_div.show();
+	if(item_cont_div.text() == "") {
+		$('#popup').html("<p style=\"text-align:center;font-size:24px;color:#fff;margin-top:20px\">请刷新页面</p>")
+	} else {
+		$('#popup').html(item_cont_div)
+	}
+	
+	$('.close').click(function () {
+		$('#popup').hide();
+		$('#mask_shadow').hide();
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- site/jywchty/js/script.js
+++ site/jywchty/js/script.js
@@ -194,48 +194,6 @@ $(document).ready(function () {
         });
     }
 
-	//百度地图服务
-	//创建和初始化地图函数：
-    function initMap(){
-      createMap();//创建地图
-      setMapEvent();//设置地图事件
-      addMapControl();//向地图添加控件
-      addMapOverlay();//向地图添加覆盖物
-    }
-    function createMap(){ 
-      map = new BMap.Map("map"); 
-      map.centerAndZoom(new BMap.Point(116.06347,35.148645),13);
-	  
-	  //创建图标
-	var pt = new BMap.Point(116.100,35.132);
-	var myIcon = new BMap.Icon("img/here.png", new BMap.Size(300,300));
-	var marker2 = new BMap.Marker(pt,{icon:myIcon});  // 创建标注
-	map.addOverlay(marker2);  
-	  
-    }
-    function setMapEvent(){
-      map.enableScrollWheelZoom();
-      map.enableKeyboard();
-      map.enableDragging();
-      map.enableDoubleClickZoom()
-    }
-    function addClickHandler(target,window){
-      target.addEventListener("click",function(){
-        target.openInfoWindow(window);
-      });
-    }
-    function addMapOverlay(){
-    }
-    //向地图添加控件
-    function addMapControl(){
-      var scaleControl = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_LEFT});
-      map.addControl(scaleControl);
-      var navControl = new BMap.NavigationControl({anchor:BMAP_ANCHOR_TOP_LEFT,type:BMAP_NAVIGATION_CONTROL_LARGE});
-      map.addControl(navControl);
-      var overviewControl = new BMap.OverviewMapControl({anchor:BMAP_ANCHOR_BOTTOM_RIGHT,isOpen:true});
-      map.addControl(overviewControl);
-    }
-    var map;
-      initMap();
+
 
 });
