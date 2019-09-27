e756ca6014d3e19dbb69abfc300833e8e8668431
gaohr <gaohrgao@163.com>
2019-09-27 23:20:57 +0800

    update

---
+++ site/special/2019/2019-09-27-idle-goods/js/itemlist.js
@@ -0,0 +1,14 @@
+var itemlist = [
+	
+	{"id":"006", "name":"牛仔外套", "describe":"太平鸟女款牛仔外套，160码，只穿了两次。", "type":"cloth", "cls":"服饰", "brand":"太平鸟", "material":"纯棉", "hownew":9.5, "seller":"boyataxia", "price":80, "postage":"包邮", "img":["img006", "img006-1"], "status":"sell", "avatar":"a032"},
+	
+	{"id":"005", "name":"棉衣", "describe":"艾格女款棉衣，160码，加棉。", "type":"cloth", "cls":"服饰", "brand":"艾格", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":150, "postage":"包邮", "img":["img005"], "status":"sell", "avatar":"a032"},
+	
+	{"id":"004", "name":"棉衣", "describe":"女款丝绒棉衣，160码，宽松版，实拍照片", "type":"cloth", "cls":"服饰", "brand":"无", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":130, "postage":"包邮", "img":["img004"], "status":"sell", "avatar":"a032"},
+	
+	{"id":"003", "name":"羽绒服", "describe":"耐克女款羽绒服，160码，保暖性强", "type":"cloth", "cls":"服饰", "brand":"耐克", "material":"羽绒", "hownew":9, "seller":"boyataxia", "price":120, "postage":"包邮", "img":["img003"], "status":"sellout", "avatar":"a032"},
+	
+	{"id":"002", "name":"风衣", "describe":"红袖女款风衣,160码，实拍照片", "type":"cloth", "cls":"服饰", "brand":"红袖", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":80, "postage":"包邮", "img":["img002"], "status":"sell", "avatar":"a032"},
+	
+	{"id":"001", "name":"衬衣", "describe":"长袖女款衬衣，V领，160码，适合秋天穿", "type":"cloth", "cls":"服饰", "brand":"明星同款", "material":"50%棉", "hownew":9, "seller":"boyataxia", "price":65, "postage":"包邮", "img":["img001", "img001-1", "img001-2"], "status":"sell", "avatar":"a032"}
+]
--- site/special/js/topicslist.js
+++ site/special/js/topicslist.js
@@ -1,12 +1,14 @@
 var topicslist = [
 
-{"topicid":"2019092401", "title":"在线代码着色高亮工具", "type":"工具", "content":"多种风格代码着色高亮，撰写技术博文必备工具。", "img":"2019/images/CodeHighlight.jpg", "date":"2019-09-24", "href":"2019/2019-09-24-code-highlight.html", "comments":"0"},
+// {"topicid":"2019092701", "title":"闲置物品交易", "type":"其他", "content":"个人及周边闲置物品，置换、交易", "img":"2019/images/IdleGoods.jpg", "date":"2019-09-27", "href":"2019/2019-09-27-idle-goods.html", "comments":"0"},
+
+{"topicid":"2019092401", "title":"在线代码着色高亮工具", "type":"工具", "content":"多种风格代码着色高亮，撰写技术博文必备工具", "img":"2019/images/CodeHighlight.jpg", "date":"2019-09-24", "href":"2019/2019-09-24-code-highlight.html", "comments":"0"},
 
 {"topicid":"2019091401", "title":"全国各省30m DEM数据", "type":"数据", "content":"全国30m分辨率DEM数据检索、下载", "img":"2019/images/ChinaDEM.jpg", "date":"2019-09-14", "href":"2019/2019-09-14-dem-30m-cn.html", "comments":"0"},
 
-{"topicid":"2019082301", "title":"开心一刻--短视频", "type":"收藏", "content":"令人捧腹的短视频，打开看看放松自己", "img":"2019/images/FunnyVideos.jpg", "date":"2019-08-23", "href":"2019/2019-08-23-funny-videos.html", "comments":"0"},
+{"topicid":"2019082301", "title":"开心一刻--短视频", "type":"娱乐", "content":"令人捧腹的短视频，时常打开看看放松一下", "img":"2019/images/FunnyVideos.jpg", "date":"2019-08-23", "href":"2019/2019-08-23-funny-videos.html", "comments":"0"},
 
-{"topicid":"2018082501", "title":"走遍中国", "type":"收藏", "content":"五岳奇景，桂林山水，传世建筑……组成了神州大地美不胜收的地理画卷！", "img":"2018/images/GeoChina.jpg", "date":"2018-08-25", "href":"2018/2018-08-25-geo-china.html", "comments":"0"},
+{"topicid":"2018082501", "title":"走遍中国", "type":"兴趣", "content":"五岳奇景，桂林山水，传世建筑……组成了神州大地美不胜收的地理画卷！", "img":"2018/images/GeoChina.jpg", "date":"2018-08-25", "href":"2018/2018-08-25-geo-china.html", "comments":"0"},
 
 {"topicid":"2018072101", "title":"雾海拾珠", "type":"情怀", "content":"云雾人生，犹如沧海，听潮起潮落，见人往人来，铭记触动心灵的每一刻！", "img":"2018/images/ShortNotes.jpg", "date":"2018-07-21", "href":"2018/2018-07-21-short-notes.html", "comments":"0"},
 
@@ -16,7 +18,7 @@ var topicslist = [
 
 {"topicid":"2017041701", "title":"冰与火之歌：九大家族", "type":"兴趣", "content":"A Song of Ice and Fire", "img":"2017/images/GameofThrones.jpg", "date":"2017-04-17", "href":"2017/2017-04-17-nine-families.html", "comments":"0"},
 
-{"topicid":"2016070801", "title":"视频收藏", "type":"收藏", "content":"一些小视频", "img":"2016/images/Videos.jpg", "date":"2016-07-08", "href":"2016/2016-07-08-videos.html", "comments":"0"},
+{"topicid":"2016070801", "title":"视频收藏", "type":"收藏", "content":"一些个人喜欢的小视频", "img":"2016/images/Videos.jpg", "date":"2016-07-08", "href":"2016/2016-07-08-videos.html", "comments":"0"},
 
 {"topicid":"2018010801", "title":"兔毛价格走势图", "type":"其他", "content":"兔毛价格走势一览", "img":"2018/images/Rabbit.jpg", "date":"2018-01-08", "href":"2018/2018-01-04-price-of-rabbit.html", "comments":"0"},
 ]
