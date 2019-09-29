b70748988e5f98a46e948e2a3a39986285f98bb3
gaohr <gaohrgao@163.com>
2019-09-29 13:03:48 +0800

    update

--- site/special/2019/2019-09-27-idle-goods/js/itemlist.js
+++ site/special/2019/2019-09-27-idle-goods/js/itemlist.js
@@ -2,11 +2,11 @@ var itemlist = [
 	
 	{"id":"006", "name":"牛仔外套", "describe":"太平鸟女款牛仔外套，160码，只穿了两次。", "type":"cloth", "cls":"服饰", "brand":"太平鸟", "material":"纯棉", "hownew":9.5, "seller":"boyataxia", "price":80, "postage":"包邮", "img":["img006", "img006-1"], "status":"sell", "avatar":"a032"},
 	
-	{"id":"005", "name":"棉衣", "describe":"艾格女款棉衣，160码，加棉。", "type":"cloth", "cls":"服饰", "brand":"艾格", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":150, "postage":"包邮", "img":["img005"], "status":"sell", "avatar":"a032"},
+	{"id":"005", "name":"棉衣", "describe":"艾格女款棉衣，160码，加棉。", "type":"cloth", "cls":"服饰", "brand":"艾格", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":150, "postage":"包邮", "img":["img005"], "status":"sellout", "avatar":"a032"},
 	
 	{"id":"004", "name":"棉衣", "describe":"女款丝绒棉衣，160码，宽松版，实拍照片", "type":"cloth", "cls":"服饰", "brand":"无", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":130, "postage":"包邮", "img":["img004"], "status":"sell", "avatar":"a032"},
 	
-	{"id":"003", "name":"羽绒服", "describe":"耐克女款羽绒服，160码，保暖性强", "type":"cloth", "cls":"服饰", "brand":"耐克", "material":"羽绒", "hownew":9, "seller":"boyataxia", "price":120, "postage":"包邮", "img":["img003"], "status":"sellout", "avatar":"a032"},
+	{"id":"003", "name":"羽绒服", "describe":"耐克女款羽绒服，160码，保暖性强", "type":"cloth", "cls":"服饰", "brand":"耐克", "material":"羽绒", "hownew":9, "seller":"boyataxia", "price":120, "postage":"包邮", "img":["img003"], "status":"sell", "avatar":"a032"},
 	
 	{"id":"002", "name":"风衣", "describe":"红袖女款风衣,160码，实拍照片", "type":"cloth", "cls":"服饰", "brand":"红袖", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":80, "postage":"包邮", "img":["img002"], "status":"sell", "avatar":"a032"},
 	
