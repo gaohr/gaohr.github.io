8c78ad4bb964d1fd25a244234cded9b3bd50dc78
gaohr <gaohrgao@163.com>
2019-10-02 11:12:57 +0800

    update

--- site/special/2019/2019-09-27-idle-goods/js/itemlist.js
+++ site/special/2019/2019-09-27-idle-goods/js/itemlist.js
@@ -1,14 +1,16 @@
 var itemlist = [
 	
-	{"id":"006", "name":"牛仔外套", "describe":"太平鸟女款牛仔外套，160码，只穿了两次。", "type":"cloth", "cls":"服饰", "brand":"太平鸟", "material":"纯棉", "hownew":9.5, "seller":"boyataxia", "price":80, "postage":"包邮", "img":["img006", "img006-1"], "status":"sell", "avatar":"a032"},
+	{"id":"007", "name":"游泳板", "describe":"儿童游泳浮板，小号背漂，建议80斤以下儿童使用", "type":"life", "cls":"家居", "brand":"无", "material":"环保EVA", "hownew":9.5, "seller":"boyataxia", "price":1, "postage":"付邮送", "img":["img007", "img007-1"], "status":"sell", "avatar":"a029"},
 	
-	{"id":"005", "name":"棉衣", "describe":"艾格女款棉衣，160码，加棉。", "type":"cloth", "cls":"服饰", "brand":"艾格", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":150, "postage":"包邮", "img":["img005"], "status":"sellout", "avatar":"a032"},
+	{"id":"006", "name":"牛仔外套", "describe":"太平鸟女款牛仔外套，160码，只穿了两次。", "type":"cloth", "cls":"服饰", "brand":"太平鸟", "material":"纯棉", "hownew":9.5, "seller":"boyataxia", "price":69, "postage":"包邮", "img":["img006", "img006-1"], "status":"sell", "avatar":"a032"},
 	
-	{"id":"004", "name":"棉衣", "describe":"女款丝绒棉衣，160码，宽松版，实拍照片", "type":"cloth", "cls":"服饰", "brand":"无", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":130, "postage":"包邮", "img":["img004"], "status":"sell", "avatar":"a032"},
+	{"id":"005", "name":"棉衣", "describe":"艾格女款棉衣，160码，加棉。", "type":"cloth", "cls":"服饰", "brand":"艾格", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":119, "postage":"包邮", "img":["img005"], "status":"sell", "avatar":"a032"},
 	
-	{"id":"003", "name":"羽绒服", "describe":"耐克女款羽绒服，160码，保暖性强", "type":"cloth", "cls":"服饰", "brand":"耐克", "material":"羽绒", "hownew":9, "seller":"boyataxia", "price":120, "postage":"包邮", "img":["img003"], "status":"sell", "avatar":"a032"},
+	{"id":"004", "name":"棉衣", "describe":"女款丝绒棉衣，160码，宽松版，实拍照片", "type":"cloth", "cls":"服饰", "brand":"无", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":99, "postage":"包邮", "img":["img004"], "status":"sell", "avatar":"a032"},
 	
-	{"id":"002", "name":"风衣", "describe":"红袖女款风衣,160码，实拍照片", "type":"cloth", "cls":"服饰", "brand":"红袖", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":80, "postage":"包邮", "img":["img002"], "status":"sell", "avatar":"a032"},
+	{"id":"003", "name":"羽绒服", "describe":"耐克女款羽绒服，160码，保暖性强", "type":"cloth", "cls":"服饰", "brand":"耐克", "material":"羽绒", "hownew":9, "seller":"boyataxia", "price":99, "postage":"包邮", "img":["img003"], "status":"sell", "avatar":"a032"},
 	
-	{"id":"001", "name":"衬衣", "describe":"长袖女款衬衣，V领，160码，适合秋天穿", "type":"cloth", "cls":"服饰", "brand":"明星同款", "material":"50%棉", "hownew":9, "seller":"boyataxia", "price":65, "postage":"包邮", "img":["img001", "img001-1", "img001-2"], "status":"sell", "avatar":"a032"}
+	{"id":"002", "name":"风衣", "describe":"红袖女款风衣,160码，实拍照片", "type":"cloth", "cls":"服饰", "brand":"红袖", "material":"纯棉", "hownew":9, "seller":"boyataxia", "price":79, "postage":"包邮", "img":["img002"], "status":"sell", "avatar":"a032"},
+	
+	{"id":"001", "name":"衬衣", "describe":"长袖女款衬衣，V领，160码，适合秋天穿", "type":"cloth", "cls":"服饰", "brand":"明星同款", "material":"50%棉", "hownew":9, "seller":"boyataxia", "price":59, "postage":"包邮", "img":["img001", "img001-1", "img001-2"], "status":"sell", "avatar":"a032"}
 ]
