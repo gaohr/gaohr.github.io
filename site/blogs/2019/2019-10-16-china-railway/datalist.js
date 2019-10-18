61d0b530cdabba27468e6de591cfc3aef1a4c3d5
gaohr <gaohrgao@163.com>
2019-10-18 23:07:03 +0800

    update

--- site/blogs/2019/2019-10-16-china-railway/datalist.js
+++ site/blogs/2019/2019-10-16-china-railway/datalist.js
@@ -1,7 +1,8 @@
 var datalist_province = [
 	{"id":"000", "name":"北京市", "data":"railways_Beijing_201910-osm-x56rs4f6"},
 	{"id":"001", "name":"天津市", "data":"railways_Tianjin_201910-osm-as5d4s5a"},
-	{"id":"002", "name":"河北省", "data":"railways_Hebei_201910-osm-5g6xsv48"}
+	{"id":"002", "name":"河北省", "data":"railways_Hebei_201910-osm-5g6xsv48"},
+	{"id":"003", "name":"山东省", "data":"railways_Shandong_201910-osm-s6d4gf86"}
 ]
 
 var datalist_city = [
