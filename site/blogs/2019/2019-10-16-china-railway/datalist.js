7b405380877492eaecdde03cedfd472470508e10
gaohr <gaohrgao@163.com>
2019-10-22 11:44:26 +0800

    update

--- site/blogs/2019/2019-10-16-china-railway/datalist.js
+++ site/blogs/2019/2019-10-16-china-railway/datalist.js
@@ -4,10 +4,14 @@ var datalist_province = [
 	{"id":"002", "name":"河北省", "data":"railways_Hebei_201910-osm-5g6xsv48"},
 	{"id":"003", "name":"山东省", "data":"railways_Shandong_201910-osm-s6d4gf86"},
 	{"id":"004", "name":"甘肃省", "data":"railways_Gansu_201910-osm-3d5f16es"},
-	{"id":"005", "name":"新疆维吾尔族自治区", "data":"railways_Xinjiang_201910-osm-s3ryk87d"}
+	{"id":"005", "name":"新疆维吾尔族自治区", "data":"railways_Xinjiang_201910-osm-s3ryk87d"},
+	{"id":"006", "name":"上海市", "data":"railways_Shanghai_201910-osm-df635yj4"},
+	{"id":"007", "name":"重庆市", "data":"railways_Chongqing_201910-osm-srh489db"}
 ]
 
 var datalist_city = [
 	{"id":"000", "name":"北京市", "data":"railways_Beijing_201910-osm-x56rs4f6"},
-	{"id":"001", "name":"天津市", "data":"railways_Tianjin_201910-osm-as5d4s5a"}
+	{"id":"001", "name":"天津市", "data":"railways_Tianjin_201910-osm-as5d4s5a"},
+	{"id":"002", "name":"上海市", "data":"railways_Shanghai_201910-osm-df635yj4"},
+	{"id":"003", "name":"重庆市", "data":"railways_Chongqing_201910-osm-srh489db"}
 ]
