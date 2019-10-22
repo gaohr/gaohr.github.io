3e4a9f50a2e2baec1d053700e1cb525b9269516d
gaohr <gaohrgao@163.com>
2019-10-22 11:57:15 +0800

    update

--- js/paylist.js
+++ js/paylist.js
@@ -1,5 +1,6 @@
 var paylist = [
 
+{"id":"32", "name":"d*y", "amount":"5.00", "date":"2019-10-22", "msg":"鼓励一下！"},
 {"id":"31", "name":"**延", "amount":"5.00", "date":"2019-10-21", "msg":"十分感谢"},
 {"id":"30", "name":"*soud", "amount":"10.00", "date":"2019-10-14", "msg":"Very good"},
 {"id":"29", "name":"*志安", "amount":"5.00", "date":"2019-10-10", "msg":"谢谢~"},
