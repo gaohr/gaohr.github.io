cf71fad2842b2bc4822e32ef2a01da2ef7d32432
gaohr <gaohrgao@163.com>
2019-10-25 08:58:23 +0800

    update

--- site/blogs/2019/2019-10-16-china-railway/datalist.js
+++ site/blogs/2019/2019-10-16-china-railway/datalist.js
@@ -1,19 +1,20 @@
 var datalist_province = [
-	{"id":"000", "name":"北京市", "data":"railways_Beijing_201910-osm-x56rs4f6"},
-	{"id":"001", "name":"天津市", "data":"railways_Tianjin_201910-osm-as5d4s5a"},
-	{"id":"002", "name":"河北省", "data":"railways_Hebei_201910-osm-5g6xsv48"},
-	{"id":"003", "name":"山东省", "data":"railways_Shandong_201910-osm-s6d4gf86"},
-	{"id":"004", "name":"甘肃省", "data":"railways_Gansu_201910-osm-3d5f16es"},
-	{"id":"005", "name":"新疆维吾尔族自治区", "data":"railways_Xinjiang_201910-osm-s3ryk87d"},
-	{"id":"006", "name":"上海市", "data":"railways_Shanghai_201910-osm-df635yj4"},
-	{"id":"007", "name":"重庆市", "data":"railways_Chongqing_201910-osm-srh489db"},
-	{"id":"008", "name":"江苏省", "data":"railways_Jiangsu_201910-osm-9lz1gse8"},
-	{"id":"009", "name":"黑龙江省", "data":"railways_Heilongjiang_201910-osm-aejr16sr"}
+	{"id":"01", "name":"北京市", "data":"railways_Beijing_201910-osm-x56rs4f6"},
+	{"id":"02", "name":"天津市", "data":"railways_Tianjin_201910-osm-as5d4s5a"},
+	{"id":"03", "name":"河北省", "data":"railways_Hebei_201910-osm-5g6xsv48"},
+	{"id":"04", "name":"山东省", "data":"railways_Shandong_201910-osm-s6d4gf86"},
+	{"id":"05", "name":"甘肃省", "data":"railways_Gansu_201910-osm-3d5f16es"},
+	{"id":"06", "name":"新疆维吾尔族自治区", "data":"railways_Xinjiang_201910-osm-s3ryk87d"},
+	{"id":"07", "name":"上海市", "data":"railways_Shanghai_201910-osm-df635yj4"},
+	{"id":"08", "name":"重庆市", "data":"railways_Chongqing_201910-osm-srh489db"},
+	{"id":"09", "name":"江苏省", "data":"railways_Jiangsu_201910-osm-9lz1gse8"},
+	{"id":"10", "name":"黑龙江省", "data":"railways_Heilongjiang_201910-osm-aejr16sr"},
+	{"id":"11", "name":"贵州省", "data":"railways_Guizhou_201910-osm-ae65grs5"}
 ]
 
 var datalist_city = [
-	{"id":"000", "name":"北京市", "data":"railways_Beijing_201910-osm-x56rs4f6"},
-	{"id":"001", "name":"天津市", "data":"railways_Tianjin_201910-osm-as5d4s5a"},
-	{"id":"002", "name":"上海市", "data":"railways_Shanghai_201910-osm-df635yj4"},
-	{"id":"003", "name":"重庆市", "data":"railways_Chongqing_201910-osm-srh489db"}
+	{"id":"001", "name":"北京市", "data":"railways_Beijing_201910-osm-x56rs4f6"},
+	{"id":"002", "name":"天津市", "data":"railways_Tianjin_201910-osm-as5d4s5a"},
+	{"id":"003", "name":"上海市", "data":"railways_Shanghai_201910-osm-df635yj4"},
+	{"id":"004", "name":"重庆市", "data":"railways_Chongqing_201910-osm-srh489db"}
 ]
