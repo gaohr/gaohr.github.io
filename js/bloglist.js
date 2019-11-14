6f05b031acfea79b5cdba0827f9a6aa6468c66eb
gaohr <gaohrgao@163.com>
2019-11-14 09:01:31 +0800

    update

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,6 +1,6 @@
 var bloglist = [
 
-{"blogid":"2019111101", "title":"不得不了解的中国基本地理国情", "type":"奇闻趣问", "author":"<a href='http://gaohr.win/'>GaoHR</a>", "content":"[GIS day 特别篇] 中国领土南北跨越的纬度近50度，东西跨越经度超过60度，陆地面积约960万平方公里，领海约470万平方公里。作为一名地理学科学生、学者、工作者，祖国的基本地理国情，不得不了解一下", "img":"site/blogs/2019/images/ChinaGeo.jpg", "date":"2019-11-13", "href":"http://gaohr.win/site/blogs/2019/22019-11-11-geographical-situation-of-cn.html", "tags":["中国", "地理", "国情"]},
+{"blogid":"2019111101", "title":"不得不了解的中国基本地理国情", "type":"奇闻趣问", "author":"<a href='http://gaohr.win/'>GaoHR</a>", "content":"[GIS day 特别篇] 中国领土南北跨越的纬度近50度，东西跨越经度超过60度，陆地面积约960万平方公里，领海约470万平方公里。作为一名地理学科学生、学者、工作者，祖国的基本地理国情，不得不了解一下", "img":"site/blogs/2019/images/ChinaGeo.jpg", "date":"2019-11-13", "href":"http://gaohr.win/site/blogs/2019/2019-11-11-geographical-situation-of-cn.html", "tags":["中国", "地理", "国情"]},
 
 {"blogid":"2019101601", "title":"全国铁路网矢量数据集（精细）", "type":"数据资源", "author":"<a href='http://gaohr.win/'>GaoHR</a>", "content":"全国精细铁路网矢量数据集，数据来源于OSM，质量较好，属性完整，供免费下载", "img":"site/blogs/2019/images/Railways.jpg", "date":"2019-10-16", "href":"http://gaohr.win/site/blogs/2019/2019-10-16-china-railway.html", "tags":["数据", "矢量", "铁路网"]},
 
