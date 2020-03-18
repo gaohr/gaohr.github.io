8670cca4157579bf9e3400cd6637fe2b877f87b4
gaohr <gaohrgao@163.com>
2020-03-18 18:21:36 +0800

    update

--- site/special/2020/2020-03-15-covid-19-cn/layers/CNcity_style.js
+++ site/special/2020/2020-03-15-covid-19-cn/layers/CNcity_style.js
@@ -5,14 +5,20 @@ var ranges_CNcity = [[-1.000000, 0.000000, [ new ol.style.Style({
 [1.000000, 9.000000, [ new ol.style.Style({
         stroke: new ol.style.Stroke({color: "rgba(55,55,55,0.25)", width: 0}) , fill: new ol.style.Fill({color: "rgba(255,240,215,1.0)"})
     })]],
-[9.000000, 99.000000, [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: "rgba(55,55,55,0.25)", width: 0}) , fill: new ol.style.Fill({color: "rgba(255,205,145,1.0)"})
+[9.000000, 49.000000, [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: "rgba(55,55,55,0.25)", width: 0}) , fill: new ol.style.Fill({color: "rgba(255,215,185,1.0)"})
     })]],
-[99.000000, 499.000000, [ new ol.style.Style({
+[49.000000, 99.000000, [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: "rgba(55,55,55,0.25)", width: 0}) , fill: new ol.style.Fill({color: "rgba(255,195,145,1.0)"})
+    })]],
+[99.000000, 249.000000, [ new ol.style.Style({
         stroke: new ol.style.Stroke({color: "rgba(55,55,55,0.25)", width: 0}) , fill: new ol.style.Fill({color: "rgba(250,155,100,1.0)"})
     })]],
-[499.000000, 999.000000, [ new ol.style.Style({
+[249.000000, 499.000000, [ new ol.style.Style({
         stroke: new ol.style.Stroke({color: "rgba(55,55,55,0.25)", width: 0}) , fill: new ol.style.Fill({color: "rgba(245,125,80,1.0)"})
+    })]],
+[499.000000, 999.000000, [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: "rgba(55,55,55,0.25)", width: 0}) , fill: new ol.style.Fill({color: "rgba(225,95,50,1.0)"})
     })]],
 [999.000000, 4999.000000, [ new ol.style.Style({
         stroke: new ol.style.Stroke({color: "rgba(55,55,55,0.25)", width: 0}) , fill: new ol.style.Fill({color: "rgba(200,35,25,1.0)"})
