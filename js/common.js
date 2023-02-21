15e734b59b6ee3d7ee7fd5d2ec21fc5bd63b5a96
gaohr <gaohrgao@163.com>
2023-02-21 09:43:24 +0800

    update

--- js/common.js
+++ js/common.js
@@ -457,34 +457,29 @@ function findblog(key, bloglist,n, type) {
 // Pageview statistics
 function PageViews() {
 	// 统计
-	
 	var g_pv = 0;
 	var cur_link = window.location.href;
 	$.ajax({url:"http://123.56.254.70:8080/gispie/PageView?link=" + encodeURI(encodeURI(cur_link)), async:false, success:function(res) {g_pv = parseInt(res)}});
-	/*
+	
 	if($(".blogtopinfo").length > 0) {
 		$(".blogtopinfo").append("<span style='margin-left:10px;'><i class='icon-eye-open'></i> 本文阅读量 <b><span class='g-color-green'>" + g_pv + "</span></b></span><br><hr>");
 	}
 	if($(".topictopinfo").length > 0) {
 		$(".topictopinfo").append("<span style='margin-left:10px;'>专题访问量 <b><span class='g-color-green'>" + g_pv + "</span></b></span>");
 	}
-	*/
+	
 	
 	// busuanzi
+	$(".body").append("<script async src='//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'></script>");
+
 	/*
 	if($(".blogtopinfo").length > 0) {
-		$(".blogtopinfo").append("<script async src='//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'></script>");
+		$(".blogtopinfo").append("<span id='busuanzi_container_page_pv' style='margin-left:10px;'><i class='icon-eye-open'></i> 本文阅读量 <b><span class='g-color-green' id='busuanzi_value_page_pv'></span></b> 次</span><br><hr>");
 	}
 	if($(".topictopinfo").length > 0) {
-		$(".topictopinfo").append("<script async src='//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'></script>");
+		$(".topictopinfo").append("<span id='busuanzi_container_page_pv' style='margin-left:10px;'>专题访问量 <b><span class='g-color-green' id='busuanzi_value_page_pv'></span></b> 次</span>");
 	}
 	*/
-	if($(".blogtopinfo").length > 0) {
-		$(".blogtopinfo").append("<script async src='//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'></script><span id='busuanzi_container_page_pv' style='margin-left:10px;'><i class='icon-eye-open'></i> 本文阅读量 <b><span class='g-color-green' id='busuanzi_value_page_pv'></span></b> 次</span><br><hr>");
-	}
-	if($(".topictopinfo").length > 0) {
-		$(".topictopinfo").append("<script async src='//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'></script><span id='busuanzi_container_page_pv' style='margin-left:10px;'>专题访问量 <b><span class='g-color-green' id='busuanzi_value_page_pv'></span></b> 次</span>");
-	}
 }
 	
 function ParticalEffect() {
