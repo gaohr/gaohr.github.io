2b2141deb15b3ecbd98e8519c44e8dad37f84924
gaohr <gaohrgao@163.com>
2023-03-08 17:23:32 +0800

    update

--- site/special/js/topicslist.js
+++ site/special/js/topicslist.js
@@ -4,19 +4,19 @@ var topicslist = [
 
 {"topicid":"2019091401", "title":"全国30m DEM数据", "type":"数据", "content":"全国30m分辨率DEM数据检索、下载", "img":"2019/images/ChinaDEM.jpg", "date":"2019-09-14", "href":"http://gaohr.win/site/special/2019/2019-09-14-dem-30m-cn.html", "comments":"0"},
 
-{"topicid":"2020022901", "title":"全球地震速报", "type":"地图", "content":"国内3级以上和国外5级以上历史和最新地震记录展示", "img":"2020/images/Earthquake.jpg", "date":"2020-02-29", "href":"http://gaohr.win/site/special/2020/2020-02-29-earthquake-info.html", "comments":"0"},
+{"topicid":"2020022901", "title":"全球地震记录", "type":"地图", "content":"国内3级以上和国外5级以上历史和最新地震记录展示", "img":"2020/images/Earthquake.jpg", "date":"2020-02-29", "href":"http://gaohr.win/site/special/2020/2020-02-29-earthquake-info.html", "comments":"0"},
 
 {"topicid":"2019092401", "title":"在线代码着色高亮工具", "type":"工具", "content":"多种风格代码着色高亮，撰写技术博文必备工具", "img":"2019/images/CodeHighlight.jpg", "date":"2019-09-24", "href":"http://gaohr.win/site/special/2019/2019-09-24-code-highlight.html", "comments":"0"},
 
 {"topicid":"2016051101", "title":"冰与火之歌：世界地图", "type":"地图", "content":"A Song of Ice and Fire", "img":"2016/images/GOTMap.jpg", "date":"2016-05-11", "href":"http://gaohr.win/site/special/2016/2016-05-11-agot-map.html", "comments":"17"},
 
-{"topicid":"2019101401", "title":"伊朗自然风情", "type":"旅行", "content":"探秘伊朗自然风情（感谢国际友人供稿）", "img":"2019/images/IranScenery.jpg", "date":"2019-10-14", "href":"http://gaohr.win/site/special/2019/2019-10-14-iran-scenery.html", "comments":"0"},
+{"topicid":"2019101401", "title":"伊朗自然风情", "type":"旅行", "content":"探秘伊朗自然风情（感谢国际友人提供素材）", "img":"2019/images/IranScenery.jpg", "date":"2019-10-14", "href":"http://gaohr.win/site/special/2019/2019-10-14-iran-scenery.html", "comments":"0"},
 
 // {"topicid":"2019082301", "title":"开心一刻--短视频", "type":"娱乐", "content":"令人捧腹的短视频，时常打开看看放松一下", "img":"2019/images/FunnyVideos.jpg", "date":"2019-08-23", "href":"http://gaohr.win/site/special/2019/2019-08-23-funny-videos.html", "comments":"0"},
 
 {"topicid":"2018082501", "title":"走遍中国", "type":"旅行", "content":"五岳奇景，桂林山水，传世建筑……组成了神州大地美不胜收的地理画卷！", "img":"2018/images/GeoChina.jpg", "date":"2018-08-25", "href":"http://gaohr.win/site/special/2018/2018-08-25-geo-china.html", "comments":"0"},
 
-{"topicid":"2018072101", "title":"雾海拾珠", "type":"情怀", "content":"云雾人生，犹如沧海，听潮起潮落，见人往人来，铭记触动心灵的每一刻！", "img":"2018/images/ShortNotes.jpg", "date":"2018-07-21", "href":"http://gaohr.win/site/special/2018/2018-07-21-short-notes.html", "comments":"0"},
+// {"topicid":"2018072101", "title":"雾海拾珠", "type":"情怀", "content":"云雾人生，犹如沧海，听潮起潮落，见人往人来，铭记触动心灵的每一刻！", "img":"2018/images/ShortNotes.jpg", "date":"2018-07-21", "href":"http://gaohr.win/site/special/2018/2018-07-21-short-notes.html", "comments":"0"},
 
 // {"topicid":"2018072102", "title":"第三场浮生", "type":"情怀", "content":"第一场是白天，第二场是黑夜，第三场属于他……！", "img":"2018/images/TheThirdLife.jpg", "date":"2018-07-21", "href":"http://gaohr.win/site/special/2018/2018-07-21-the-third-life.html", "comments":"0"},
 
@@ -27,7 +27,7 @@ var topicslist = [
 /* others */
 // {"topicid":"2019092701", "title":"闲置物品交易", "type":"其他", "content":"个人及周边闲置物品，置换、交易", "img":"2019/images/IdleGoods.jpg", "date":"2019-09-27", "href":"http://gaohr.win/site/special/2019/2019-09-27-idle-goods.html", "comments":"0"},
 
-{"topicid":"2018010801", "title":"兔毛价格走势图", "type":"其他", "content":"兔毛价格走势一览", "img":"2018/images/Rabbit.jpg", "date":"2018-01-08", "href":"http://gaohr.win/site/special/2018/2018-01-04-price-of-rabbit.html", "comments":"0"},
+// {"topicid":"2018010801", "title":"兔毛价格走势图", "type":"其他", "content":"兔毛价格走势一览", "img":"2018/images/Rabbit.jpg", "date":"2018-01-08", "href":"http://gaohr.win/site/special/2018/2018-01-04-price-of-rabbit.html", "comments":"0"},
 ]
 
 
